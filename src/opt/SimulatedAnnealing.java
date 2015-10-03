b9d93dff2cf4913799573026f91703fb72a5e4f5
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-03 15:32:32 -0600

    Update SimulatedAnnealing.java

--- src/opt/SimulatedAnnealing.java
+++ src/opt/SimulatedAnnealing.java
@@ -1,71 +1,37 @@
-package opt;
+#/**
+# * A simulated annealing hill climbing algorithm
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
+class SimulatedAnnealing: # extends OptimizationAlgorithm {
+#    /**
+#     * Make a new simulated annealing hill climbing
+#     * @param t the starting temperature
+#     * @param cooling the cooling exponent
+#     * @param hcp the problem to solve
+#     */
+    def __init__(double t, double cooling, HillClimbingProblem hcp):
+#        super(hcp)
+        self.t = t
+        self.cooling = cooling
+        self.cur = hcp.random()
+        self.curVal = hcp.value(cur)
 
-import dist.Distribution;
+#    /**
+#     * @see shared.Trainer#train()
+#     */
+    def train(self):
+        HillClimbingProblem p = (HillClimbingProblem) getOptimizationProblem()
+        Instance neigh = p.neighbor(cur)
+        double neighVal = p.value(neigh)
+        if (neighVal > self.curVal || Distribution.random.nextDouble() < Math.exp((neighVal - self.curVal) / self.t)):
+            self.curVal = neighVal
+            self.cur = neigh
+        self.t = self.t * cooling
+        return self.curVal
 
-import shared.Instance;
-
-/**
- * A simulated annealing hill climbing algorithm
- * @author Andrew Guillory gtg008g@mail.gatech.edu
- * @version 1.0
- */
-public class SimulatedAnnealing extends OptimizationAlgorithm {
-    
-    /**
-     * The current optimiation data
-     */
-    private Instance cur;
-    
-    /**
-     * The current optimization value
-     */
-    private double curVal;
-    
-    /**
-     * The current temperature
-     */
-    private double t;
-    
-    /**
-     * The cooling parameter
-     */
-    private double cooling;
-    
-    /**
-     * Make a new simulated annealing hill climbing
-     * @param t the starting temperature
-     * @param cooling the cooling exponent
-     * @param hcp the problem to solve
-     */
-    public SimulatedAnnealing(double t, double cooling, HillClimbingProblem hcp) {
-        super(hcp);
-        this.t = t;
-        this.cooling = cooling;
-        this.cur = hcp.random();
-        this.curVal = hcp.value(cur);
-    }
-
-    /**
-     * @see shared.Trainer#train()
-     */
-    public double train() {
-        HillClimbingProblem p = (HillClimbingProblem) getOptimizationProblem();
-        Instance neigh = p.neighbor(cur);
-        double neighVal = p.value(neigh);
-        if (neighVal > curVal || Distribution.random.nextDouble() < 
-                Math.exp((neighVal - curVal) / t)) {
-            curVal = neighVal;
-            cur = neigh;
-        }
-        t *= cooling;
-        return curVal;
-    }
-
-    /**
-     * @see opt.OptimizationAlgorithm#getOptimal()
-     */
-    public Instance getOptimal() {
-        return cur;
-    }
-
-}
+#    /**
+#     * @see opt.OptimizationAlgorithm#getOptimal()
+#     */
+    def getOptimal():
+        return cur
