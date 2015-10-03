5ae42aea62c2bc88a6b4f2081439479a589ee1f2
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-03 15:24:16 -0600

    Update and rename NeighborFunction.java to NeighborFunction.py

---
+++ src/opt/NeighborFunction.py
@@ -0,0 +1,13 @@
+#/**
+# * A neighbor function
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
+class NeighborFunction:
+#    /**
+#     * Get the neighbor of a piece of data
+#     * @param d the data
+#     * @return the neighbor
+#     */
+    def neighbor(Instance d):
+        print ""
