f0c4057a6109ace886ce3b9d460780c90310335c
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-03 15:16:25 -0600

    Update and rename EvaluationFunction.java to EvaluationFunction.py

---
+++ src/opt/EvaluationFunction.py
@@ -0,0 +1,14 @@
+#/**
+# * A class representing an evaluation function
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
+class EvaluationFunction:
+    
+#    /**
+#     * Evaluate a data
+#     * @param d the data to evaluate
+#     * @return the value
+#     */
+    def value(Instance d):
+        print ""
