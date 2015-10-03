d0d12ef31dec5d3e34a6d12c0b064667e87157e9
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-03 15:26:23 -0600

    Update and rename OptimizationAlgorithm.java to OptimizationAlgorithm.py

---
+++ src/opt/OptimizationAlgorithm.py
@@ -0,0 +1,27 @@
+#/**
+# * An abstract class for optimzation algorithms
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
+class OptimizationAlgorithm: # implements Trainer {
+#    /**
+#     * Make a new optimization algorithm
+#     * @param op the problem to optimize
+#     */
+    def __init__(OptimizationProblem op) {
+        this.op = op;
+    }
+    
+#    /**
+#     * Get an optimization problem
+#     * @return the problem
+#     */
+    def getOptimizationProblem():
+        return op
+    
+#    /**
+#     * Get the optimal data
+#     * @return the data
+#     */
+    def getOptimal():
+        print ""
