fa6bff7e773eb345334ababfaaaf8fb192b1c466
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-03 15:10:37 -0600

    Update OptimizationProblem.py

--- src/opt/OptimizationProblem.py
+++ src/opt/OptimizationProblem.py
@@ -1,20 +1,20 @@
 
-/**
- * A class representing an optimization problem.
- * @author Andrew Guillory gtg008g@mail.gatech.edu
- * @version 1.0
- */
+#
+# * A class representing an optimization problem.
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
 Class OptimizationProblem:
-    /**
-     * Evaluate the given data
-     * @param d the data to evaluate
-     * @return the value of the data.
-     */
+#    /**
+#     * Evaluate the given data
+#     * @param d the data to evaluate
+#     * @return the value of the data.
+#     */
     def value(Instance d):
        print ""
-    /**
-     * Draw a random sample of optimization data.
-     * @return the sampled data.
-     */
+#    /**
+#     * Draw a random sample of optimization data.
+#     * @return the sampled data.
+#     */
     def random():
        print ""
