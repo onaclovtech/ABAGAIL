fcd9e2b04c8d9efb8a068953280239beeed2bd1a
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-03 15:13:34 -0600

    Update and rename GenericOptimizationProblem.java to GenericOptimizationProblem.py

---
+++ src/opt/GenericOptimizationProblem.py
@@ -0,0 +1,29 @@
+#/**
+# * A generic continuous optimization problem
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
+Class GenericOptimizationProblem:  # Inherit? OptimizationProblem {
+
+#    /**
+#     * Make a new generic optimization problem
+#     * @param dist the initial distribution
+#     * @param eval the evaluation function
+#     */
+    def __init__(EvaluationFunction eval, Distribution dist):
+        self.initial = dist;
+        self.eval = eval;
+    
+
+#    /**
+#     * @see opt.OptimizationProblem#value(opt.OptimizationData)
+#     */
+    def value(Instance d)
+        return eval.value(d)
+
+
+#    /**
+#     * @see opt.OptimizationProblem#random()
+#     */
+    def random():
+        return initial.sample(null)
