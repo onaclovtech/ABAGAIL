51dcbb961197c2b0abf7f833b35469bb8bf030b7
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-03 23:39:51 -0600

    Update and rename ProbabilisticOptimizationProblem.java to ProbabilisticOptimizationProblem.py

---
+++ src/opt/prob/ProbabilisticOptimizationProblem.py
@@ -0,0 +1,14 @@
+#/**
+# * An optimization problem solvable by MIMIC
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
+class ProbabilisticOptimizationProblem(OptimizationProblem):
+    
+#    /**
+#     * Build a distribution from the given data
+#     * @param data the data to build the distribution from
+#     * @return the distrubtion
+#     */
+    def getDistribution():
+        print ""
