4e03b728ef6d3ce1e88f8768e0f5d62633295013
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-03 22:26:24 -0600

    Update GeneticAlgorithmProblem.java

--- src/opt/ga/GeneticAlgorithmProblem.java
+++ src/opt/ga/GeneticAlgorithmProblem.java
@@ -1,26 +1,22 @@
-package opt.ga;
-
-import opt.OptimizationProblem;
-import shared.Instance;
-
-/**
- * A genetic algorithm problem
- * @author Andrew Guillory gtg008g@mail.gatech.edu
- * @version 1.0
- */
-public interface GeneticAlgorithmProblem extends OptimizationProblem {
+#/**
+# * A genetic algorithm problem
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
+class GeneticAlgorithmProblem(OptimizationProblem):
     
-    /**
-     * Mate two optimization datas
-     * @param a the first one to mate
-     * @param b the second one to mate
-     * @return the result of mating them
-     */
-    public abstract Instance mate(Instance a, Instance b);
+#    /**
+#     * Mate two optimization datas
+#     * @param a the first one to mate
+#     * @param b the second one to mate
+#     * @return the result of mating them
+#     */
+    def mate(self, Instance a, Instance b):
+        print ""
     
-    /**
-     * Mutate a observation data
-     * @param d the data to mutate
-     */
-    public abstract void mutate(Instance d);
-}
+#    /**
+#     * Mutate a observation data
+#     * @param d the data to mutate
+#     */
+    def mutate(Instance d):
+        print ""
