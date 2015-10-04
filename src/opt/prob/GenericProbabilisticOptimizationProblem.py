61def1e662ca06e391f877ececc23af20f184c38
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-03 22:22:13 -0600

    Update and rename GenericProbabilisticOptimizationProblem.java to GenericProbabilisticOptimizationProblem.py

---
+++ src/opt/prob/GenericProbabilisticOptimizationProblem.py
@@ -0,0 +1,24 @@
+#/**
+# * 
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
+class GenericProbabilisticOptimizationProblem(GenericOptimizationProblem):
+#implements ProbabilisticOptimizationProblem {
+
+#    /**
+#     * Make a new generic probabilisitic optimiziation problem
+#     * @param eval the evaluation function
+#     * @param dist the initial parameter distribution
+#     * @param fact the distribution factory
+#     */
+    def __init__(EvaluationFunction eval, Distribution dist, Distribution d):
+        self.eval = eval
+        self.dist = dist
+        self.dist = d
+
+#    /**
+#     * @see opt.prob.ProbabilisticOptimizationProblem#getDistribution()
+#     */
+    def getDistribution(self):
+        return self.dist
