a11b5801f36283252144d48a8a2ff8f66d966ec2
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-04 07:08:03 -0600

    Update LayeredNetwork.java

--- src/func/nn/LayeredNetwork.java
+++ src/func/nn/LayeredNetwork.java
@@ -1,195 +1,147 @@
-package func.nn;
-import java.util.ArrayList;
-import java.util.List;
+#/**
+# * A layered neural network
+# * @author Andrew Guillory gtg008g@mail.gatech.edu
+# * @version 1.0
+# */
+class LayeredNetwork(NeuralNetwork):
 
-import util.linalg.Vector;
 
 
-/**
- * A layered neural network
- * @author Andrew Guillory gtg008g@mail.gatech.edu
- * @version 1.0
- */
-public abstract class LayeredNetwork extends NeuralNetwork {
-
-	/**
-	 * The input layer
-	 */
-	private Layer inputLayer;
 	
-	/**
-	 * The output layer
-	 */
-	private Layer outputLayer;
+#	/**
+#	 * Make a new layered network
+#	 */
+	def __init__():
+		self.hiddenLayers = [];
+		self.inputLayer = new Layer()
+		self.outputLayer = new Later()
+		self.links = null;
+
+#	/**	
+# 	 * @see Network#getOutputValues()
+# 	 */
+	def getOutputValues(self):
+		return self.outputLayer.getActivations()
 	
-	/**
-	 * The list of middle layers
-	 */
-	private List hiddenLayers;
+#	/**
+#	 * @see Network#setInputValues(double[])
+#	 */
+	def setInputValues(self, Vector values):
+		self.inputLayer.setActivations(values)
     
-    /**
-     * The cached list of links
-     */
-    private List links = null;
-	
-	/**
-	 * Make a new layered network
-	 */
-	public LayeredNetwork() {
-		hiddenLayers = new ArrayList();
-	}
-
-	/**	
- 	 * @see Network#getOutputValues()
- 	 */
-	public Vector getOutputValues() {
-		return outputLayer.getActivations();
-	}
-	
-	/**
-	 * @see Network#setInputValues(double[])
-	 */
-	public void setInputValues(Vector values) {
-		inputLayer.setActivations(values);
-	}
+#    /**
+#     * Get the index of the node with the largest value
+#     * @return the index
+#     */
+	def getDiscreteOutputValue(self):
+        	return self.outputLayer.getGreatestActivationIndex()
     
-    /**
-     * Get the index of the node with the largest value
-     * @return the index
-     */
-    public int getDiscreteOutputValue() {
-        return outputLayer.getGreatestActivationIndex();
-    }
-    
-    /**
-     * Get the binary output value
-     * @return the binary output value
-     */
-    public boolean getBinaryOutputValue() {
-        return outputLayer.getNode(0).getActivation() > .5;
-    }
+#    /**
+#     * Get the binary output value
+#     * @return the binary output value
+#     */
+    def getBinaryOutputValue(self):
+        return self.outputLayer.getNode(0).getActivation() > .5
 
-	/**
-	 * Get the input layer
-	 * @return the layer
-	 */
-	public Layer getInputLayer() {
-		return inputLayer;
-	}
+#	/**
+#	 * Get the input layer
+#	 * @return the layer
+#	 */
+	def getInputLayer(self):
+		return inputLayer
 
-	/**
-	 * Get the list of middle layers
-	 * @return the list
-	 */
-	public List getHiddenLayers() {
-		return hiddenLayers;
-	}
+#	/**
+#	 * Get the list of middle layers
+#	 * @return the list
+#	 */
+	def getHiddenLayers(self):
+		return hiddenLayers
 
-	/**
-	 * Get the output layer
-	 * @return the layer
-	 */
-	public Layer getOutputLayer() {
-		return outputLayer;
-	}
+#	/**
+#	 * Get the output layer
+#	 * @return the layer
+#	 */
+	def getOutputLayer(self):
+		return outputLayer
 
-	/**
-	 * Set the input layer
-	 * @param layer the new layer
-	 */
-	public void setInputLayer(Layer layer) {
-		inputLayer = layer;
-	}
+#	/**
+#	 * Set the input layer
+#	 * @param layer the new layer
+#	 */
+	def setInputLayer(self, layer):
+		inputLayer = layer
 
-	/**
-	 * Set the output layer
-	 * @param layer the output layer
-	 */
-	public void setOutputLayer(Layer layer) {
-		outputLayer = layer;
-	}
+#	/**
+#	 * Set the output layer
+#	 * @param layer the output layer
+#	 */
+	def setOutputLayer(self, layer):
+		outputLayer = layer
 
-	/**
-	 * Get the middle layer count
-	 * @return the middle layer count
-	 */
-	public int getHiddenLayerCount() {
-		return hiddenLayers.size();
-	}
+#	/**
+#	 * Get the middle layer count
+#	 * @return the middle layer count
+#	 */
+	def getHiddenLayerCount(self):
+		return len(self.hiddenLayers)
 	
-	/**
-	 * Get the middle layer
-	 * @param i the index of the middle layer
-	 * @return the layer
-	 */
-	public Layer getHiddenLayer(int i) {
-		return (Layer) hiddenLayers.get(i);
-	}
+#	/**
+#	 * Get the middle layer
+#	 * @param i the index of the middle layer
+#	 * @return the layer
+#	 */
+	def getHiddenLayer(self, int i):
+		return self.hiddenLayers.get(i) # Returns a layer, dunno
 	
-	/**
-	 * Add a middle layer
-	 * @param layer the layer to add
-	 */
-	public void addHiddenLayer(Layer layer) {
-		hiddenLayers.add(hiddenLayers.size(), layer);
-	}
+#	/**
+#	 * Add a middle layer
+#	 * @param layer the layer to add
+#	 */
+	def addHiddenLayer(self, layer):
+		self.hiddenLayers.add(hiddenLayers.size(), layer)
 
-	/**
-	 * Disconnect this network
-	 */
-	public void disconnect() {
-		if (inputLayer != null && getHiddenLayerCount() > 0) {
-			Layer firstMiddle = getHiddenLayer(0);
-			inputLayer.disconnect(firstMiddle);
-		} else if (inputLayer != null && outputLayer != null) {
-			inputLayer.disconnect(outputLayer);
-		}
-		for (int i = 0; i + 1 < getHiddenLayerCount(); i++) {
-			Layer first = getHiddenLayer(i);
-			Layer second = getHiddenLayer(i + 1);
-			first.disconnect(second);
-		}
-		if (outputLayer != null && getHiddenLayerCount() > 0) {
-			Layer lastMiddle = getHiddenLayer(getHiddenLayerCount() - 1);
-			lastMiddle.disconnect(outputLayer);
-		}
-	}
-	
-	/**
-	 * Connect this network
-	 */
-	public void connect() {
-		if (inputLayer != null && getHiddenLayerCount() > 0) {
-			Layer firstMiddle = getHiddenLayer(0);
-			inputLayer.connect(firstMiddle);
-		} else if (inputLayer != null && outputLayer != null) {
-			inputLayer.connect(outputLayer);
-		}
-		for (int i = 0; i + 1 < getHiddenLayerCount(); i++) {
-			Layer first = getHiddenLayer(i);
-			Layer second = getHiddenLayer(i + 1);
-			first.connect(second);
-		}
-		if (outputLayer != null && getHiddenLayerCount() > 0) {
-			Layer lastMiddle = getHiddenLayer(getHiddenLayerCount() - 1);
-			lastMiddle.connect(outputLayer);
-		}
-	}
-    
-    /**
-     * @see nn.NeuralNetwork#getLinks()
-     */
-    public List getLinks() {
-       if (links != null) {
-           return links;
-       }
-       links = new ArrayList();
-       links.addAll(inputLayer.getLinks());
-       for (int i = 0; i < getHiddenLayerCount(); i++) {
-           links.addAll(getHiddenLayer(i).getLinks()); 
-       }
-       links.addAll(outputLayer.getLinks());
-       return links;
-    }
-
-}
+#	/**
+#	 * Disconnect this network
+#	 */
+	def disconnect(self) {
+		if (self.inputLayer != null && self.getHiddenLayerCount() > 0):
+			Layer firstMiddle = self.getHiddenLayer(0)
+			self.inputLayer.disconnect(firstMiddle)
+		else:
+			if (self.inputLayer != null && self.outputLayer != null):
+				self.inputLayer.disconnect(self.outputLayer)
+		for (i = 0; i + 1 < self.getHiddenLayerCount(); i++):
+			Layer first = self.getHiddenLayer(i)
+			Layer second = self.getHiddenLayer(i + 1)
+			first.disconnect(second)
+		if (self.outputLayer != null && self.getHiddenLayerCount() > 0):
+			Layer lastMiddle = getHiddenLayer(getHiddenLayerCount() - 1)
+			lastMiddle.disconnect(outputLayer)
+#	/**
+#	 * Connect this network
+#	 */
+	def connect(self) {
+		if (self.inputLayer != null && self.getHiddenLayerCount() > 0):
+			Layer firstMiddle = self.getHiddenLayer(0)
+			self.inputLayer.connect(firstMiddle)
+		else:
+			if (self.inputLayer != null && self.outputLayer != null):
+				self.inputLayer.connect(self.outputLayer)
+		for (int i = 0; i + 1 < self.getHiddenLayerCount(); i++):
+			Layer first = self.getHiddenLayer(i)
+			Layer second = self.getHiddenLayer(i + 1)
+			first.connect(second)
+		if (self.outputLayer != null && self.getHiddenLayerCount() > 0):
+			Layer lastMiddle = self.getHiddenLayer(self.getHiddenLayerCount() - 1)
+			lastMiddle.connect(self.outputLayer)
+#    /**
+#     * @see nn.NeuralNetwork#getLinks()
+#     */
+    def getLinks(self):
+       if (self.links != null):
+           return self.links
+       self.links.addAll(self.inputLayer.getLinks())
+       for (int i = 0; i < self.getHiddenLayerCount(); i++):
+           self.links.addAll(self.getHiddenLayer(i).getLinks())
+       self.links.addAll(self.outputLayer.getLinks())
+       return links
