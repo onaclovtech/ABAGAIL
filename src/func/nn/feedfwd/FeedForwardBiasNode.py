3378e6381844244049d93df32c4d643a162dc2ea
Tyson Bailey <onaclov2000+github@gmail.com>
2015-10-04 14:06:01 -0600

    Update and rename FeedForwardBiasNode.java to FeedForwardBiasNode.py

---
+++ src/func/nn/feedfwd/FeedForwardBiasNode.py
@@ -0,0 +1,15 @@
+#/**
+# * 
+# * @author Jesse Rosalia <https://github.com/theJenix>
+# * @date 2013-03-05
+# */
+class FeedForwardBiasNode(FeedForwardNode):
+
+    def __init__(activation):
+        self.setActivation(activation)
+
+#/**
+#* @see nn.FeedForwardNode#feedforward()
+#*/
+    def feedforward():
+        print ""
